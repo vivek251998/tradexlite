d94d434f4a817457bc8c19eda9a2e55a46a06ee7
vivek251998 <vivek2508intellect@gmail.com>
2026-03-23 13:19:32 +0530

    commit main

--- lib/app/modules/auth/login_controller.dart
+++ lib/app/modules/auth/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:local_auth/local_auth.dart';
 
@@ -17,7 +18,32 @@ class LoginController extends GetxController {
         Get.offAllNamed('/market');
       }
     } catch (e) {
-      Get.snackbar("Error", e.toString());
+      String message = "Authentication failed";
+
+      if (e is PlatformException) {
+        switch (e.code) {
+          case 'NotAvailable':
+            message = "Biometric not available on this device";
+            break;
+          case 'NotEnrolled':
+            message = "No biometrics enrolled";
+            break;
+          case 'LockedOut':
+            message = "Too many attempts. Try later";
+            break;
+          case 'PermanentlyLockedOut':
+            message = "Biometric locked permanently";
+            break;
+          default:
+            message = "Error: ${e.message}";
+        }
+      }
+
+      Get.snackbar(
+        "Error",
+        message,
+        snackPosition: SnackPosition.BOTTOM,
+      );
     }
   }
 }
